5ca1dce1893f96997c362b6d6ce06ba3ff142487
王煜琳 <q1063496374@163.com>
2019-10-21 20:49:32 +0800

    修改登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,55 +1,60 @@
 <template>
-  <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
+  <el-container class="container">
+    <el-header class="header">
+	  <span class="title">慢病管理工作平台</span>
+	</el-header>
+    <el-main class="content">
+	  <div class="login-container">
+        <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-width="20px">
 
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
+          <div class="title-container">
+            <h3 class="title">用户登录</h3>
+          </div>
 
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
+          <el-form-item prop="username">
+            <span class="svg-container">
+              <svg-icon icon-class="user" />
+            </span>
+            <el-input
+              ref="username"
+              v-model="loginForm.username"
+              placeholder="Username"
+              name="username"
+              type="text"
+              tabindex="1"
+              auto-complete="on"
+            />
+          </el-form-item>
 
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="password"
+              v-model="loginForm.password"
+              :type="passwordType"
+              placeholder="Password"
+              name="password"
+              tabindex="2"
+              auto-complete="on"
+              @keyup.enter.native="handleLogin"
+            />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
 
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+          <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
 
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
-    </el-form>
-  </div>
+        </el-form>
+	  </div>
+    </el-main>
+	<el-footer class="footer">
+	  Copyright © 2019 浙江大学
+	</el-footer>
+  </el-container>
 </template>
 
 <script>
@@ -176,18 +181,48 @@ $cursor: #fff;
 $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
+$gray:#a9a9a9;
+
+.container{
+  background: url('../../assets/images/home.jpg') center center no-repeat;
+  background-size: cover;
+  height: 100%;
+  .header{
+    background-color: gray;
+    opacity: 0.8;
+    .title{
+      font-size: 26px;
+	  color: white;
+	  line-height: 60px;
+    }
+  }
+  .content{
+    position: relative;
+	
+  }
+  .footer{
+    height: 40px;
+    text-align: center;
+  }
+}
 
 .login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
+  position:absolute;
+  top: 50%;
+  right: 100px;
+  height: 360px;
+  width: 400px;
+  margin-top: -180px;
   overflow: hidden;
+  background-color: gray;
+  opacity: 0.9;
+  border-radius: 20px;
+  
 
   .login-form {
     position: relative;
-    width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    padding: 30px 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
